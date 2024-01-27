55fe857f2d0dd827ab1db20f1a8061cbeb7a440c
tanvir35web <islammdtanvirul@gmail.com>
2024-01-28 00:00:55 +0600

    cart item numbers and price functionality added

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { getItemSelector } from "../redux/slices/cartSlice";
+
 
 const Cart = () => {
+
+    const items = useSelector(getItemSelector);
+    const totalPrice = items.reduce((a, b) => a + b.price, 0)
   return (
     <div className="alert alert-success">
-      <h3 className="text-center">Total Item 5 (Rs 3,000)/-</h3>
+      <h3 className="text-center">Total Item {items.length} (Rs {totalPrice})/-</h3>
     </div>
   );
 };
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,13 +1,17 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { addItem } from "../redux/slices/cartSlice";
 
 const Product = (props) => {
+
+    const dispatch = useDispatch();
   return (
     <div className="card" style={{ width: "18rem", margin: "10px" }}>
       <img className="card-img-top" src={props.image} alt={props.productName} />
       <div className="card-body">
         <h5 className="card-title"> {props.productName} </h5>
         <p className="card-text"> Rs. {props.price} </p>
-        <button className="btn btn-primary"> Add to cart </button>
+        <button onClick={(e) => dispatch(addItem({ name: props.productName, price: props.price }))} className="btn btn-primary"> Add to cart </button>
       </div>
     </div>
   );
